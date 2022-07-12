c3f7e43cf771856e9347ff152da9c66626ec08f2
michael <michael.chilan@epn.edu.ec>
2022-07-11 21:44:29 -0500

    Examen
    V1

---
+++ Examen_1B/app/src/main/java/com/example/examen_1b/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.examen_1b
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import io.sentry.Sentry
+import io.sentry.SentryLevel
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Sentry.captureMessage("testing SDK setup", SentryLevel.INFO)
+        Sentry.captureMessage("testing SDK setup", SentryLevel.INFO)
+
+        val botonVerJuegos = findViewById<Button>(R.id.btn_ver_juegos)
+        botonVerJuegos.setOnClickListener {
+            irActividad(Juego::class.java)
+        }
+    }
+
+    fun irActividad (
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
+}
