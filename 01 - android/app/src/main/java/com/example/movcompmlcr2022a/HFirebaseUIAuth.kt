96a600cb529eca04c6ae004b5950d1cfd095fdab
michael <michael.chilan@epn.edu.ec>
2022-08-14 23:34:35 -0500

    Clase 16
    Cloud Firebase

--- 01 - android/app/src/main/java/com/example/movcompmlcr2022a/HFirebaseUIAuth.kt
+++ 01 - android/app/src/main/java/com/example/movcompmlcr2022a/HFirebaseUIAuth.kt
@@ -8,6 +8,9 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HFirebaseUIAuth : AppCompatActivity() {
     // See: https://developer.android.com/training/basics/intents/result
@@ -52,6 +55,33 @@ class HFirebaseUIAuth : AppCompatActivity() {
         val btnLogout = findViewById<Button>(R.id.btn_logout)
         btnLogin.visibility = View.INVISIBLE
         btnLogout.visibility = View.VISIBLE
+        if(res.isNewUser == true){
+            registrarUsuarioPorPrimeraVez(res)
+        }
+    }
+    fun registrarUsuarioPorPrimeraVez(usuario: IdpResponse){
+        val usuarioLogueado = FirebaseAuth.getInstance().currentUser
+        if(usuario.email != null && usuarioLogueado != null){
+            val db = Firebase.firestore //obtenemos referencia
+            val roles = arrayListOf("usuario") // creamos el arreglo de permisos
+            val email = usuarioLogueado.email // correo
+            val uid = usuarioLogueado.uid //identificador
+            val nuevoUsuario = hashMapOf<String, Any>( // { roles:... uid:...}
+                "roles" to roles,
+                "uid" to uid,
+                "email" to email.toString()
+            )
+            db.collection("usuario")
+                // .document() // Me crea el identificador el Firestore
+                .document(email.toString()) // El identificador lo setea ya
+                .set(nuevoUsuario)
+                .addOnSuccessListener {
+                    // Ya se crea el usuario en Firestore
+                }
+                .addOnFailureListener{
+                    // Hubo errores creando usuario en Firestore
+                }
+        }
     }
     fun seDeslogueo(){
         val btnLogin = findViewById<Button>(R.id.btn_intent_firebase_ui)
