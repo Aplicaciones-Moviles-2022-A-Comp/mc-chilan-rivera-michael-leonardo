39ef2d5342cd14bffdc0432476c4b3c911b250d4
michael <michael.chilan@epn.edu.ec>
2022-08-30 14:44:25 -0500

    Case 21
    Parte Final

--- 01 - android/app/src/main/java/com/example/movcompmlcr2022a/FGoogleMaps.kt
+++ 01 - android/app/src/main/java/com/example/movcompmlcr2022a/FGoogleMaps.kt
@@ -3,17 +3,17 @@ package com.example.movcompmlcr2022a
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PackageManagerCompat
-
 import com.google.android.gms.maps.CameraUpdateFactory
+
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.movcompmlcr2022a.databinding.ActivityFgoogleMapsBinding
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class FGoogleMaps : AppCompatActivity() {
 
@@ -33,8 +33,62 @@ class FGoogleMaps : AppCompatActivity() {
             if (googleMap != null) {
                 mapa = googleMap
                 establecerConfiguracionMapa()
+
+                val quicentro = LatLng(-0.176125, -78.480208)
+                val titulo = "Quicentro"
+                val zoom = 17f
+                anadirMarcador(quicentro, titulo)
+                moverCamaraConZoom(quicentro, zoom)
+
+                // LINEA
+                val poliLineaUno = googleMap
+                    .addPolyline(
+                        PolylineOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1759187040647396, -78.48506472421384),
+                                LatLng(-0.17632468492901104, -78.48265589308046),
+                                LatLng(-0.17746143130181483, -78.4770533307815)
+                            )
+                    )
+                poliLineaUno.tag = "linea-1" // <- ID
+
+                // POLIGONO
+                val poligonoUno = googleMap
+                    .addPolygon(
+                        PolygonOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1771546902239471, -78.48344987495214),
+                                LatLng(-0.17968981486125768, -78.48269198043828),
+                                LatLng(-0.17710958124147777, -78.48142892291516)
+                            )
+                    )
+                poligonoUno.fillColor = -0xc771c4
+                poligonoUno.tag = "poligono-2" // <- ID
+
+                escucharListeners()
             }
         }
+    }
+
+    fun escucharListeners() {
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolyLineClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnMarkerClickListener {
+            Log.i("mapa", "setOnMarkerClickListener ${it}")
+            it.tag // ID
+                return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {  }
+        mapa.setOnCameraMoveStartedListener {  }
+        mapa.setOnCameraIdleListener {  }
     }
 
     fun establecerConfiguracionMapa() {
@@ -52,6 +106,21 @@ class FGoogleMaps : AppCompatActivity() {
             uiSettings.isZoomControlsEnabled = true
             uiSettings.isMyLocationButtonEnabled = true // no tenemos permisos aun
         }
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String) {
+        mapa.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )
+    }
+
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f) {
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng, zoom)
+        )
     }
 
     fun solicitarPermisos() {
